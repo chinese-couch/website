01c116c57a70979ee5230615ab5b1d3bca08cc75
鍾益飛 <ifeichung@gmail.com>
2025-05-25 02:24:06 +0800

    Remove garbage

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once(__DIR__ . '/google-api/vendor/autoload.php');
-$google_client = new Google\Client();
-$google_client->setAuthConfig(__DIR__ . '/client_secret.json');
-$google_client->addScope("email");
-$google_client->addScope("profile");
-session_start();
-?>
